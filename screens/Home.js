ec06b398e30781ac39f2d8991cecde51a4506287
Alina <alinakassym@gmail.com>
2021-10-07 12:42:32 +0600

    get UpcomingMovies Images

--- screens/Home.js
+++ screens/Home.js
@@ -4,12 +4,22 @@ import {
   Text,
   View,
 } from 'react-native';
-import {getPopularMovies} from '../services/services'
+import {getPopularMovies, getUpcomingMovies} from '../services/services'
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
+  const [moviesImages, setMoviesImages] = useState([]);
   const [error, setError] = useState(false);
   useEffect(() => {
+    getUpcomingMovies()
+      .then(items => {
+        const imgArr = [];
+        items.forEach(item => {
+          imgArr.push(`https://image.tmdb.org/t/p/w500${item.poster_path}`)
+        })
+        setMoviesImages(imgArr);
+        console.log('moviesImages', moviesImages);
+    });
     getPopularMovies()
       .then(res => {
         setMovies(res[0]);
@@ -20,6 +30,7 @@ const Home = () => {
   }, []);
   return (
     <View style={styles.block}>
+      <Text>Home</Text>
       <Text style={styles.text}>Movie Name: {movies.original_title}</Text>
       <Text style={styles.text}>Language: {movies.original_language}</Text>
       <Text style={styles.text}>Release date: {movies.release_date}</Text>
